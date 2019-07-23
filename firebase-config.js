571516be1adb911e6a7ca70a5651d8e12039fd09
julniter <40856322+julniter@users.noreply.github.com>
2019-07-23 14:41:23 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCd55dh-Zot03-MY1YGui8P4POGKXxE9kE",
+  authDomain: "cs-sample-project.firebaseapp.com",
+  databaseURL: "https://cs-sample-project.firebaseio.com",
+  projectId: "cs-sample-project",
+  storageBucket: "cs-sample-project.appspot.com",
+  messagingSenderId: "714931911632",
+  appId: "1:714931911632:web:b1117e6fac0c8702"
+};
